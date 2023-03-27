d89638064cc4528e675338074f18962e08bcdb97
thor <thor@ubuntu.com>
2023-03-27 12:48:41 +0800

    RequestHandler切入点

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@
 import tornado.web
 import tornado.ioloop
 import tornado.options
+import json
 from tornado.httputil import HTTPServerRequest
 
 
@@ -11,14 +12,14 @@ class IndexHandler(tornado.web.RequestHandler):
         # 请求参数读取
         # get_argument和get_arguments在GET,POST...请求方式中都通用
         # get_query_argument 和 get_query_arguments 只适用于GET请求的查询参数
-
+        # get请求参数要全部传递对，否则报400 bad request 错误
         # 1.读取单个参数
         wd = self.get_argument('wd')
         print(wd)
         # 2.读取多个参数名相同的参数值
         titles = self.get_arguments("title")
         print(titles)
-        self.write("<h3>Hello Tornado</h3>")
+        # self.write("<h3>Hello Tornado</h3>")
         # 3.从查询参数中读取url路径参数
         wd2 = self.get_query_argument('wd')
         print(wd2)
@@ -36,8 +37,17 @@ class IndexHandler(tornado.web.RequestHandler):
 
     def post(self):
         """提交数据"""
+        # 读取表单参数
+        # 方式1：任何请求方式都可以获取
+        # name = self.get_argument('name')
+        # city = self.get_argument('city')
+        # 方式2：在Post请求时建议使用这种
         name = self.get_body_argument('name')
         city = self.get_body_argument('city')
+        # 方式3：
+        req = self.request
+        name = req.arguments.get('name').decode('utf-8')
+        city = req.arguments.get('city').decode('utf-8')
         print(name, city)
         self.write("<h3>Post请求</h3>")
 
@@ -49,9 +59,138 @@ class IndexHandler(tornado.web.RequestHandler):
         """提交数据"""
         self.write("<h3>Delete请求</h3>")
 
+
+class SearchHandler(tornado.web.RequestHandler):
+    mapper = {
+        'python': 'no.1',
+        'java': 'no.2'
+    }
+
+    def get(self):
+        html = """
+            <h3>搜索 %s 结果</h3>
+            <p>
+                %s
+            </p>
+        """
+        wd = self.get_query_argument('wd')
+        result = self.mapper.get(wd)
+
+        # self.write(html % (wd, result))
+        resp_result = {
+            'wd': wd,
+            'result': result
+        }
+        self.write(json.dumps(resp_result))
+        self.set_header("Content-Type", "application/json;charset=utf-8")  # 设置响应头
+        self.set_status(200)  # 设置响应状态码
+        # 设置cookie
+        self.set_cookie("wd", wd)
+
+
+class CookieHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        # 从查询参数中读取Cookie的名称
+        if self.request.arguments.get('name'):
+            name = self.get_query_argument('name')
+            print("name:", name)
+            # 如果cookie的键不存在，返回None
+            cookie = self.get_cookie(name)
+            self.write(cookie)
+        else:
+            # 显示所有cookie
+            cookies = self.request.cookies
+            html = '<ul>%s</ul>'
+            cookie_list = []
+            for cookie in cookies.keys():
+                cookie_list.append("<li>%s: %s</li>" % (cookie, self.get_cookie(cookie)))
+            self.write(html % ''.join(cookie_list))
+
+    def delete(self):
+        """删除cookie"""
+        name = self.get_argument("name")
+        if self.request.cookies.get(name, None):
+            # 删除cookie
+            self.clear_cookie(name)
+            self.write("<h3 style='color:green'>删除cookie %s 成功</h3>" % name)
+        else:
+            self.write("<h3 style='color:red'>删除cookie %s 失败</h3>" % name)
+
+        # 重定向操作时，不需要再调用self.write(),即使调用了也不会在浏览器上显示
+        self.redirect("/cookie")
+
+
+class OrderHandler(tornado.web.RequestHandler):
+    goods = [
+        {
+            "id": "1",
+            "name": "哈利波特",
+            "author": "JK罗琳",
+            "price": 180
+        },
+        {
+            "id":"2",
+            "name": "龙族",
+            "author": "江南",
+            "price": 80
+        }
+
+    ]
+
+    action_map = {
+        1: "取消订单",
+        2: "再次购买",
+        3: "评价"
+    }
+
+    def query(self, order_id):
+        for good in self.goods:
+            if good.get("id") == order_id:
+                return good
+
+    def initialize(self):
+        # 所有的请求方法在调用之前，都会进行初始化操作
+        print("-----initialize-----")
+
+    def prepare(self):
+        # 在初始化之后，调用行为方法之前，调用此方法进行预处理
+        print("-----prepare-----")
+
+    def on_finish(self):
+        # 在行为方法调用之后调用，可用于释放资源
+        print("--on_finish--")
+
+
+    def get(self, order_id, action_code):
+        html = """
+            <p>
+                商品编号：%s
+            </p>
+            <p>
+                商品名称：%s
+            </p>
+                商品价格：%s
+            <p>
+            </p>
+        """
+        print("---Get---")
+        good = self.query(order_id)
+        self.write("<h3>订单查询</h3>")
+        self.write(html % (good.get("id"), good.get("name"), good.get("price")))
+        self.write(self.action_map.get(int(action_code)))
+
+    def post(self, order_id, action_code):
+        print("---POST---")
+        self.write("---POST---")
+
+
 def make_app():
     return tornado.web.Application([
-        ('/', IndexHandler)
+        ('/', IndexHandler),
+        ('/search', SearchHandler),
+        ('/cookie', CookieHandler),
+        (r'/order/(?P<order_id>\d+)/(?P<action_code>\d+)', OrderHandler)
     ], default_host=tornado.options.options.host)
 
 if __name__ == '__main__':
--- test_method.py
+++ test_method.py
@@ -7,7 +7,20 @@ import requests
 class TestTornadoRequest(TestCase):
     base_url = "http://192.168.3.19:9000"
 
+    def test_index_get(self):
+        """测试get请求"""
+        url = self.base_url + '/'
+
+        # 查询参数
+        resp = requests.get(url, params={
+            'wd': 'tornado',
+            # 多个相同参数名，值用列表传递
+        })
+        # 可能会出现400错误，原因是参数没给对
+        print(resp.text)
+
     def test_index_post(self):
+        """测试Post请求"""
         url = self.base_url + '/'
         resp = requests.post(url, data={
             'name': 'thor',
@@ -16,4 +29,34 @@ class TestTornadoRequest(TestCase):
         print(resp.text)
 
 
+class TestCookieRequest(TestCase):
+    base_url = "http://192.168.3.19:9000"
 
+    def text_cookie_delete(self):
+        url = self.base_url + '/cookie'
+        resp = requests.delete(url, params={
+            'name': 'wd'
+        })
+        print(resp.txt)
+
+
+class TestOrderRequest(TestCase):
+    base_url = "http://192.168.3.19:9000/order"
+
+    def test_get(self):
+        resp = requests.get(self.base_url + "/1/1")
+        print(resp.text)
+
+    def test_post(self):
+        resp = requests.post(self.base_url + "/1/1")
+        print(resp.text)
+
+
+class TestUserRequest(TestCase):
+    url = "http://192.168.3.19:8000/user"
+
+    def test_login(self):
+        # 上传json数据
+        resp = requests.get(self.url, json={"name": "thor", "pwd": "123"})
+        # 获取响应的json数据
+        print(resp.json())
